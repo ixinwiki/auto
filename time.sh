0101c8ac8221d6fdd73c370c8791b3110dbdb7ef
wilinz <weiizan71@gmail.com>
2024-02-03 23:28:18 +0800

    update time

---
+++ time.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 获取当前时间的小时和分钟
+current_hour=$(date +"%H")
+current_minute=$(date +"%M")
+
+# 计算当前时间与最接近的6的倍数之间的时间差
+remainder=$(((current_hour + 1) % 6))
+hours_to_next_6=$((6 - remainder))
+if [ $remainder -eq 0 ]; then
+    hours_to_next_6=0
+fi
+
+# 计算分钟的时间差
+if [ $current_minute -gt 0 ]; then
+    minutes_to_next_6=$((60 - current_minute - 2))
+else
+    minutes_to_next_6=0
+fi
+
+# 输出时间差
+echo "${hours_to_next_6}h ${minutes_to_next_6}m"
