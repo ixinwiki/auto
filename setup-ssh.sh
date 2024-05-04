83c0b7ab24f665f1585c4d227e7a23a9439b7ca4
ixinwiki <106441119+ixinwiki@users.noreply.github.com>
2024-05-05 05:49:08 +0800

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -6,7 +6,8 @@ sudo apt update && sudo apt install openssh-server -y
 # Start SSH service
 sudo systemctl start ssh
 
-# Set up SSH connection
+echo "Set up SSH connection"
+
 echo 'root:$1' | sudo chpasswd
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo service ssh restart
