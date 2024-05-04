3bf7daead62c2983aeb27b4fb0b42e4ea1923ed3
ixinwiki <106441119+ixinwiki@users.noreply.github.com>
2024-05-05 06:00:21 +0800

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -6,10 +6,16 @@ sudo apt update && sudo apt install openssh-server -y
 echo "Start SSH service"
 sudo systemctl start ssh
 
-echo "Set up SSH connection"
+echo "Set up SSH connection using public key from environment variable"
 
-echo 'root:$SSH_PASSWD' | sudo chpasswd
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# 从环境变量中获取公钥
+PUBLIC_KEY=$SSH_PUBLIC_KEY
+
+# 将公钥添加到授权密钥中
+echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
+
+# 禁用密码登录
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 sudo service ssh restart
 
 echo "Check SSH service status"
